63845e6afa58f08c28492d4f759f066a0a5de72b
andrew dillon <majik895@gmail.com>
2019-10-23 18:06:28 -0400

    added auth0

--- onboarding/src/App.js
+++ onboarding/src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
-import './App.css';
+// src/App.js
+
+import React from "react";
+import NavBar from "./auth0/NavBar.jsx";
+
+
+// New - import the React Router components, and the Profile page component
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Profile from "./components/Profile";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
     <div className="App">
-
+      {/* New - use BrowserRouter to provide access to /profile */}
+      <BrowserRouter>
+        <header>
+          <NavBar />
+        </header>
+        <Switch>
+          <Route path="/" exact />
+          <PrivateRoute path="/profile" component={Profile} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
