4008e7ffa65f774b3162739fcc845a8ae36d2551
andrew dillon <majik895@gmail.com>
2019-10-24 13:25:15 -0400

    updated navbar

--- onboarding/src/auth0/NavBar.jsx
+++ onboarding/src/auth0/NavBar.jsx
@@ -1,26 +1,65 @@
-import React from 'react'
-import { useAuth0 } from './react-auth0-spa'
-import { Link } from 'react-router-dom'
+import React, { Component } from 'react'
+import $ from 'jquery'
+import '../styles/nav.scss'
+import Button from '../components/GoogleButton/Button.jsx'
 
-const NavBar = () => {
-	const { isAuthenticated, loginWithRedirect, logout } = useAuth0()
+export default class NavBar extends Component {
+	componentDidMount() {
+		$(document).ready(function() {
+			$('.mobile-button a').click(function(e) {
+				$(this)
+					.parent()
+					.parent()
+					.toggleClass('open')
+				$(this).html($(this).html() === 'Close Menu' ? 'Menu' : 'Close Menu')
+				e.preventDefault()
+			})
+		})
+	}
 
-	return (
-		<div>
-			{!isAuthenticated && (
-				<button onClick={() => loginWithRedirect({})}>Log in</button>
-			)}
-			{isAuthenticated && <button onClick={() => logout()}>Log out</button>}
-
-			{/* NEW - add a link to the home and profile pages */}
-			{isAuthenticated && (
-				<span>
-					<Link to='/'>Home</Link>&nbsp;
-					<Link to='/profile'>Profile</Link>
-				</span>
-			)}
-		</div>
-	)
+	render() {
+		return (
+			<div>
+				<header class='header'>
+					<div class='container'>
+						<h1 class='site-title'>Super Cool Web App!</h1>
+						<span class='site-tagline'>Because Andy made this!</span>
+					</div>
+				</header>
+				<nav class='main-nav'>
+					<div class='container'>
+						<ul>
+							<li class='mobile-button'>
+								<a href='/'>Menu</a>
+							</li>
+							<li>
+								<a href='/'>About</a>
+							</li>
+							<li>
+								<Button />
+							</li>
+							<li>
+								<a href='/'>Methods</a>
+							</li>
+							<li>
+								<a href='/'>Results</a>
+							</li>
+							<li>
+								<a href='/'>Contact</a>
+							</li>
+							<li>
+								<a href='/'>Methods</a>
+							</li>
+							<li>
+								<a href='/'>Results</a>
+							</li>
+							<li>
+								<a href='/'>Contact</a>
+							</li>
+						</ul>
+					</div>
+				</nav>
+			</div>
+		)
+	}
 }
-
-export default NavBar
