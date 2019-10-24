33bc1cbfcc70fde24e6eef65de7776778944173c
Ryan-Wisniewski <Ryanwis98@yahoo.com>
2019-10-24 00:55:07 -0400

    added form in SignUp_In

--- onboarding/src/App.js
+++ onboarding/src/App.js
@@ -2,10 +2,10 @@ import React from "react";
 import NavBar from "./auth0/NavBar.jsx";
 import { useAuth0 } from "./auth0/react-auth0-spa"
 
-
 // New - import the React Router components, and the Profile page component
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Profile from "./components/Profile";
+import SignUp_In from './components/SignUp_In'
 import PrivateRoute from "./components/PrivateRoute";
 
 
@@ -30,7 +30,7 @@ function App() {
           <NavBar />
         </header>
         <Switch>
-          <Route path="/" exact />
+          <Route path="/" exact component={SignUp_In}/>
           <PrivateRoute path="/profile" component={Profile} />
         </Switch>
       </BrowserRouter>
