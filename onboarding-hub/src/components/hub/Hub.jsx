aae47eb90fb9dd038db974831d2bb472c9f1fcf6
Chris York <christopherayork1992@gmail.com>
2019-10-25 18:16:11 -0400

    more styling, added jquery CDN to public/index.html, added to styles.js and tried to get a carousel going in Hub.jsx

--- onboarding-hub/src/components/hub/Hub.jsx
+++ onboarding-hub/src/components/hub/Hub.jsx
@@ -5,12 +5,21 @@ import StoreSideBar from './StoreSidebar';
 import Footer from '../Footer';
 import { HubDiv} from "../../styles/styled-components/styles";
 import FireflyExample from '../../assets/fireflyexample.png';
+import Firefly from '../../assets/firefly.png';
+import OwlCarousel from 'react-owl-carousel';
+
 const gameLink = 'https...';
 
+const CarouselConfig = {
+  className: 'carousel',
+  loop: true,
+  nav: true,
+  items: 1,
+  autoWidth: true,
+};
 
 const Hub = props => {
   // when we get more than one game, add the logic for switching between game store pages.
-
   return (
     <HubDiv className='hub-container'>
       <main>
@@ -18,13 +27,17 @@ const Hub = props => {
         <article>
           <div className='game-info'>
             <h1 className='game-name'>Firefly</h1>
-            <img src={FireflyExample} alt='firefly example' />
+            <OwlCarousel {...CarouselConfig}>
+              <img src={FireflyExample} alt='firefly example' />
+              <img src={Firefly} alt='blah' />
+            </OwlCarousel>
             <button className='play-now'><a href={`${gameLink}`}>Play Firefly</a></button>
           </div>
         </article>
       </main>
       <Footer/>
     </HubDiv>
+
   );
 };
 
