d43d0a23f82fe22bb8412d7940ce2ac7c6348ba8
Chris York <christopherayork1992@gmail.com>
2019-10-30 18:01:48 -0400

    part of the hub matching the figma

--- onboarding-hub/src/styles/styled-components/styles.js
+++ onboarding-hub/src/styles/styled-components/styles.js
@@ -1,37 +1,37 @@
 import styled from 'styled-components';
 import Palette from "../Palette";
-// import FireflyExample from '../../assets/IconsBackground.png';
+import IBG from '../../assets/IconsBackground.png';
 
 export const HubDiv = styled.div`
-  width: 100%;
   height: 87vh;
-  background: white;
+  background: ${Palette.purple};
   box-sizing: border-box;
-  margin: 0;
+  margin: 0 auto;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  
   main {
-    width: 100%;
+    width: 99%;
     display: flex;
-    height: 70vh;
+    flex-direction: column;
+    height: 80vh;
     
     @media (max-width: 500px) {
       flex-direction: column;
     }
     
     aside.sidebar {
-      width: 10%;
+      width: 100%;
       box-sizing: border-box;
-      padding: 10px;
-      border: 2px solid ${Palette.darkpurple};
       background: ${Palette.purple};
       color: white;
       
       @media (max-width: 800px) {
-        width: 20%;
+        //width: 20%;
       }
       
       @media (max-width: 500px) {
-        width: 100vw;
-        height: 10%;
       }
       
       div.game {
@@ -41,7 +41,6 @@ export const HubDiv = styled.div`
         display: flex;
         flex-direction: column;
         align-items: center;
-        padding: 10px 0;
         
         @media (max-width: 500px) {
           flex-direction: row;
@@ -50,7 +49,7 @@ export const HubDiv = styled.div`
         
         div.firefly-hub {
           width: 70%;
-          height: auto;
+          //height: auto;
           text-align: center;
           padding-bottom: 3px;
           
@@ -64,6 +63,7 @@ export const HubDiv = styled.div`
             border-radius: 50%;
             box-shadow: 0 3px 0 ${Palette.darkpurple};
             height: auto;
+            max-height: 100px;
             
             @media (max-width: 500px) {
               max-height: 50px;
@@ -74,27 +74,24 @@ export const HubDiv = styled.div`
     }
     
     article {
-      width: 90%;
+      width: 100%;
+      height: 60vh;
       box-sizing: border-box;
       padding: 10px;
       display: flex;
       justify-content: center;
       align-items: center;
-      border-left: 2px solid black;
-      border-bottom: 2px solid ${Palette.green};
+      border-radius: 5px 5px 0 0;
       background-color: ${Palette.lightgreen};
-     
-      background-repeat: no-repeat;
-      background-position: center;
-      background-size: cover;
+      background-image: url(${IBG});
       
       @media (max-width: 800px) {
-        width: 80%;
+        //width: 80%;
       }
       
       @media (max-width: 500px) {
-        width: 100vw;
-        height: 100%;
+        //width: 100vw;
+        //height: 100%;
       }
       
       div.game-info {
@@ -105,8 +102,8 @@ export const HubDiv = styled.div`
         display: flex;
         flex-direction: column;
         align-items: center;
-        background: ${Palette.white};
-        box-shadow: 0 10px 10px ${Palette.green};
+        //background: ${Palette.white};
+        //box-shadow: 0 10px 10px ${Palette.green};
         padding-bottom: 5px;
         
         @media (max-width: 800px) {
@@ -115,19 +112,6 @@ export const HubDiv = styled.div`
         
         @media (max-width: 500px) {
           height: 50%;
-        }
-        
-        h1.game-name {
-          margin: 20px 0;
-          color: ${Palette.darkgreen};
-          
-          @media (max-width: 500px) {
-            font-size: 1.5rem;
-          }
-          
-        }
-        div {
-          
         }
         
         img {
@@ -168,24 +152,31 @@ export const HubDiv = styled.div`
 
 export const CarouselDiv = styled.div`
   width: 100%;
-  overflow-x: scroll;
+  overflow-x: hidden;
   overflow-y: hidden;
   display: flex;
   align-items: stretch;
+  justify-content: center;
   
-  img {
-    max-width: 80%;
-    width: 80%;
-    object-fit: cover;
+  .reel {
+    display: flex;
+    align-items: center;
+    height: 100%;
     
-    &:not(.selected) {
-      display: none;
+    img {  
+      max-width: 100%;
+      width: 100%;
+      object-fit: cover;
+      
+      
+      &:not(.selected) {
+        display: none;
+      }
     }
   }
   
   .left-arrow, .right-arrow {
-    //height: 100%;
-    background-color: ${Palette.purple};
+    //background-color: ${Palette.purple};
     color: ${Palette.white};
     font-size: 3rem;
     font-weight: bold;
@@ -194,7 +185,13 @@ export const CarouselDiv = styled.div`
     padding: 0 10px;
     display: flex;
     align-items: center;
-    transition: 1s background-color color;
+    transition: background-color 1s, color 1s;
+    -webkit-text-stroke: 3px ${Palette.darkpurple};
+    
+    @media (max-width: 500px) {
+      font-size: 1rem;
+      padding: 0 5px;
+    }
     
     &:hover {
       background-color: ${Palette.darkpurple};
