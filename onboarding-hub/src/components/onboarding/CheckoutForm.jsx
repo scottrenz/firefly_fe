5574075d5e50b0ea1f427acad882b1132426615a
Elijah McKay <elijahmckay10@gmail.com>
2019-11-01 20:49:06 -0500

    stripe token creation working

--- onboarding-hub/src/components/onboarding/CheckoutForm.jsx
+++ onboarding-hub/src/components/onboarding/CheckoutForm.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { CardElement, injectStripe } from 'react-stripe-elements';
+import { CardElement, injectStripe, ReactStripeElements } from 'react-stripe-elements';
 import styled from 'styled-components'; 
 
 import Pricing from '../../../src/assets/welcomeback.png'
@@ -9,21 +9,35 @@ import '../../styles/scss/CheckoutForm.scss'
 
 class CheckoutForm extends Component {
   constructor(props) {
-    super(props);
+	super(props);
+	this.state = {
+		name: this.props.name,
+		email: '',
+		amount: ''
+	}
     this.submit = this.submit.bind(this);
   }
 
+  changeHandler = (e) => {
+	  this.setState({ [e.target.name]: e.target.value })
+  }
   async submit(ev) {
-    let {token} = await this.props.stripe.createToken({name: "Name"});
-    let response = await fetch("/charge", {
+	let {token} = await this.props.stripe.createToken({ name: this.state.name }); 
+	console.log(token); 
+    let response = await fetch("https://infinite-meadow-87721.herokuapp.com/stripe/api/stripe", {
       method: "POST",
       headers: {"Content-Type": "text/plain"},
       body: token.id
-    });
+	});
+	
+	console.log(response); 
   
     if (response.ok) console.log("Purchase Complete!")
   }
-
+  
+  componentDidMount() {
+	  console.log(this.state); 
+  }
   // styled components for stripe component
   StripeStyle = styled(CardElement)`
   	  margin: 0 auto; 
@@ -38,22 +52,22 @@ class CheckoutForm extends Component {
 		<h3>Teaching kids to code with fun!</h3>
 		<div className="body-container">
 			<div className="form-container">
-				<label className="email">Email
-					<input type="text" className="email"/>
+				<label className="email-label">Email
+					<input type="text" className="email-input" name="email" value={this.state.email} onChange={this.changeHandler} />
 				</label>
 				<p className="select-plan"><span><b>Select a membership</b></span><br />Don't worry, you can always change this later.</p>
 				<div className="radio-container">
 					<div className="monthly-container">
-						<input type="radio" id="monthly-id" className="monthly" name="plan-radio" />
-						<label htmlFor="monthly-id" class="sub-label"> Monthly - $4.99 per month</label>
+						<input type="radio" id="monthly-id" className="monthly" name="amount" value={this.state.amount} onChange={this.changeHandler} />
+						<label htmlFor="monthly-id" className="sub-label"> Monthly - $4.99 per month</label>
 					</div>
 					<div className="yearly-container">
-						<input type="radio" id="yearly-id" className="yearly" name="plan-radio" />
-						<label htmlFor="yearly-id" class="sub-label">Yearly - $49.99 per year</label>
+						<input type="radio" id="yearly-id" className="yearly" name="amount" value={this.state.amount} onChange={this.changeHandler} />
+						<label htmlFor="yearly-id" className="sub-label">Yearly - $49.99 per year</label>
 					</div>
 				</div>
 			</div>
-			<img src={Pricing} alt="pricing models" class="pricing" />
+			<img src={Pricing} alt="pricing models" className="pricing" />
 		</div>
 
 		<div className="card-info-container">
