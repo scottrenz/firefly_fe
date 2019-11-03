64eeeaf5bec75db3f370e19f3921d0d65c5fecd0
Adetunji <ashennaike@gmail.com>
2019-11-02 23:28:02 -0400

    pulled from onbaording and had to fix navbar since link was removed

--- onboarding-hub/src/components/navigation/NavBar.spec.jsx
+++ onboarding-hub/src/components/navigation/NavBar.spec.jsx
@@ -4,18 +4,19 @@ import { render } from '@testing-library/react'
 // Import renderer for snapshot
 import renderer from 'react-test-renderer'
 // Added router because there are links in the NavBar file
-import { BrowserRouter as Router } from 'react-router-dom'
+// import { BrowserRouter as Router } from 'react-router-dom'
+//Link was removed
 
 import NavBar from './NavBar'
 
 // Test to check that the app will display when called.
 describe('<NavBar />', () => {
   it('should display', () => {
-    render(<Router><NavBar/></Router>);
+    render(<NavBar/>);
   })
   // Create a snapshot
   it('matches snapshot', () => {
-    const tree = renderer.create(<Router><NavBar/></Router>); // Generates a DOM tree
+    const tree = renderer.create(<NavBar/>); // Generates a DOM tree
 
     // Snapshots are a JSON representation of the DOM tree
     expect(tree.toJSON()).toMatchSnapshot();
