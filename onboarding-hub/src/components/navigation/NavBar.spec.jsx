40a4480ba3b53e7ecaeb2035625550c3ec7fad7c
Adetunji <ashennaike@gmail.com>
2019-11-02 23:23:47 -0400

    working passed the items that are giving me a hard time and will circle back later, finished the navbar by adding router to the test

--- onboarding-hub/src/components/navigation/NavBar.spec.jsx
+++ onboarding-hub/src/components/navigation/NavBar.spec.jsx
@@ -3,17 +3,19 @@ import React from 'react'
 import { render } from '@testing-library/react'
 // Import renderer for snapshot
 import renderer from 'react-test-renderer'
+// Added router because there are links in the NavBar file
+import { BrowserRouter as Router } from 'react-router-dom'
 
-import StoreSidebar from './StoreSidebar'
+import NavBar from './NavBar'
 
 // Test to check that the app will display when called.
-describe('<StoreSidebar />', () => {
+describe('<NavBar />', () => {
   it('should display', () => {
-    render(<StoreSidebar/>) 
+    render(<Router><NavBar/></Router>);
   })
   // Create a snapshot
   it('matches snapshot', () => {
-    const tree = renderer.create(<StoreSidebar />); // Generates a DOM tree
+    const tree = renderer.create(<Router><NavBar/></Router>); // Generates a DOM tree
 
     // Snapshots are a JSON representation of the DOM tree
     expect(tree.toJSON()).toMatchSnapshot();
