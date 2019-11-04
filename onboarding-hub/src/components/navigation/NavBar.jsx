4a5621f92f900652efca74e84d780fb629655740
Adetunji <ashennaike@gmail.com>
2019-11-04 12:44:40 -0500

    pulling from master to merge and start again

--- onboarding-hub/src/components/navigation/NavBar.jsx
+++ onboarding-hub/src/components/navigation/NavBar.jsx
@@ -8,31 +8,6 @@ const Logo = require('../../assets/WearingNerdGlasses.png')
 export default class NavBar extends Component {
    render() {
       return (
-<<<<<<< HEAD
-         <nav id="navigation">
-            <a href="/" className="logo">Project FireFly<span>&copy;</span></a>
-            <span className="fire-logo"><img src={Logo} alt="Firefly Logo" style={{ width: '100px', marginTop: '-15px' }}></img></span>
-            <ul className="links">
-               <li><a className="fa fa-home fa-2x" style={{ color: 'white' }} href="/">&nbsp;</a></li>
-               <li className="dropdown"><a href="/" className="trigger-drop">Links<i className="arrow"></i></a>
-                  <ul className="drop">
-                     <li><a href="/contact">Contact</a></li>
-                     <li><a href="/blob">Empty</a></li>
-                     <li><a href="/sempty">Empty</a></li>
-                     <li><a href="/uei">Empty</a></li>
-                  </ul>
-               </li>
-               <li className="dropdown"><a href="/" className="trigger-drop">Accounts<i className="arrow"></i></a>
-                  <ul className="drop">
-                     <li><a href="/signin">Sign&nbsp;In</a></li>
-                     <li><a href="/slogin">Sign&nbsp;Up</a></li>
-                     <li><a href="/profile">Profile</a></li>
-                     <li><a href="/signout">Sign&nbsp;Out</a></li>
-                  </ul>
-               </li>
-            </ul>
-         </nav>
-=======
          <div id="header" className="header">
             <div className="max-width">
                <div className="branding-wrapper">
@@ -89,7 +64,6 @@ export default class NavBar extends Component {
                </ul>
             </div>
          </div>
->>>>>>> 826e71305a7a77bf45c12bfb343fe76ba5a759ee
       )
    }
 }
