5507d075695ba5029722c2fb4169314942fca167
Timothy McGowen <mcgowen17@yahoo.com>
2019-11-05 21:11:31 -0700

    Styled and readusted the profile page

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 
-const Profile = () => {
+
+const Profile = (props) => {
     const [checked, setChecked] = useState(false);
     const [edit, setEdit] = useState(false)
     // const [user, setUser] = useState({})
@@ -25,8 +26,7 @@ const Profile = () => {
     const user = {
         email: 'email@email.com',
         password: '123456789',
-        first_name: 'John',
-        last_name: 'Doe',
+        name: 'John Doe',
         phone_number: '101 101 1234',
         academic_research: true,
         parent_age: 20,
@@ -43,112 +43,95 @@ const Profile = () => {
         subscription: 'Monthly',
     }
     return (
-        <div className='container-profile'>
-            {/**=============== Left Side  ======================= */}
-            {edit ? <div>
-                <div className="flex">
-                    <div>
+        <div>
+            <h1 className="my-account">My Account</h1>
+            <div className='container-profile'>
+                {/**=============== Left Side  ======================= */}
+                {edit ? <div className="flex">
+                    <div className="flex-one">
                         {/**=============== Account Information  ======================= */}
-                        <div className="flex-one">
-                            <h2>Account Information</h2>
-                            <span><p>Email:</p> <input value={user.email}></input></span>
-                            <span><p>Password:</p> <input value={user.password}></input></span>
+                        <div>
+                            <div className="flex-header">
+                                <h2>Account Information</h2>
+                                <button className="circle-edit" onClick={() => setEdit(false)}>Edit</button>
+                            </div>
+                            <span><p>Email:</p> <p>{user.email}</p></span>
+                            <span><p>Name:</p> <p>{user.name}</p></span>
+                            <span><p>Password:</p> <p>{user.password}</p></span>
                         </div>
                         {/**=============== Payment Information  ======================= */}
                         <div>
                             <h2>Payment Information</h2>
                             <span><p>Subscription:</p></span>
+                            <span><p>Credit Card:</p></span>
+
                         </div>
                         {/**=============== Educational Research Participation Toggle  ======================= */}
                         <div>
                             <h2>Educational Research Information</h2>
                         </div>
                         {/**=============== Educational Research Information  ======================= */}
-                        {checked ? <div>
-                            <div>
-                                <span><p>Phone Number:</p><input value={user.phone_number}></input></span>
-                                <span><p>Academic Research:</p><input value={user.academic_research}></input></span>
-                                <span><p>Parent Age:</p><input value={user.parent_age}></input></span>
-                                <span><p>Marital Status:</p><input value={user.marital_status}></input></span>
-                                <span><p>Relation to Child:</p><input value={user.relation_to_child}></input></span>
-                                <span><p>Education:</p><input value={user.education}></input></span>
-                                <span> <p>Address: <input value={user.address}></input></p></span>
-                                <span> <p>City: <input value={user.city}></input></p></span>
-                                <span> <p>State: <input value={user.state}></input></p></span>
-                                <span> <p>country: <input value={user.country}></input></p></span>
-                                <span> <p>Zip: <input value={user.zip}></input></p></span>
-                            </div>
-                        </div> : <span><input
-                            type='checkbox'
-                            name='Educational-Research'
-                            checked={checked}
-                            onChange={handleCheck}
-                        />
-                                <p>I would like to participate in the Educational Research</p></span>}
-                    </div>
-                    {/**=============== Manage Profile  ======================= */}
-                    {/** <div className="flex-two">
-                            <div>
-                                <h2>Manage Profile</h2>
-                                <p>Place Holder Info</p>
-                            </div>
-                    </div>   */}
-                </div>
-            </div> : <div>
-                    <div className="flex">
-                        <div>
-                            {/**=============== Account Information  ======================= */}
-                            <div className="flex-one">
-                                <h2>Account Information</h2>
-                                <span><p>Email:</p> <p>{user.email}</p></span>
-                                <span><p>Password:</p> <p>{user.password}</p></span>
-                            </div>
-                            {/**=============== Payment Information  ======================= */}
-                            <div>
-                                <h2>Payment Information</h2>
-                                <span><p>Subscription:</p><p>{stripeInfo.subscription}</p></span>
-                            </div>
-                            {/**=============== Educational Research Participation Toggle  ======================= */}
-                            <div>
-                                <h2>Educational Research Information</h2>
-                            </div>
-                            {/**=============== Educational Research Information  ======================= */}
-                            {checked ? <div>
-                                <div>
-                                    <span><p>Phone Number:</p><p> {user.phone_number}</p></span>
-                                    <span><p>Academic Research:</p><p>{user.academic_research}</p></span>
-                                    <span><p>Parent Age:</p><p> {user.parent_age}</p></span>
-                                    <span><p>Marital Status:</p><p> {user.marital_status}</p></span>
-                                    <span><p>Relation to Child:</p><p> {user.relation_to_child}</p></span>
-                                    <span><p>Education:</p><p>{user.education}</p></span>
-                                    <span><p>Address: {user.address}</p></span>
-                                    <span><p>City: {user.city}</p></span>
-                                    <span><p>State: {user.state}</p></span>
-                                    <span><p>country: {user.country}</p></span>
-                                    <span> <p>Zip: {user.zip}</p></span>
-                                </div>
-                            </div> : <span><input
+                        <span>
+                            <input
                                 type='checkbox'
                                 name='Educational-Research'
                                 checked={checked}
                                 onChange={handleCheck}
                             />
-                                    <p>I would like to participate in the Educational Research</p></span>}
+                            <p className="check-box">I would like to participate in the Educational Research</p>
+                        </span>
+                    </div>
+                    {/**=============== Manage Profile  ======================= */}
+                    <div className="flex-two">
+                        <div>
+                            <h2>Manage Profile</h2>
+                            <p>Place Holder Info</p>
+                            <button onClick={() => props.history.push('/hub')}>BACK TO GAME</button>
+                        </div>
+                    </div>
+                </div> : <div className="flex">
+                        <div className="flex-one">
+                            {/**=============== Account Information  ======================= */}
+                            <div className="account-information">
+                                <div className="flex-header">
+                                    <h2>Account Information</h2>
+                                    <button className="circle-edit" onClick={() => setEdit(true)}>Edit</button>
+                                </div>
+                                <div className="span">
+                                    <div><p>Email:</p><p className="edit-field">{user.email}</p></div>
+                                    <div><p>Name:</p><p className="edit-field">{user.name}</p></div>
+                                    <div><p>Password:</p> <p className="edit-field">{user.password}</p></div>
+                                </div>
+                            </div>
+                            {/**=============== Payment Information  ======================= */}
+                            <div className="span payment-information">
+                                <h2>Payment Information</h2>
+                                <div><p>Subscription:</p><p className="edit-field">{stripeInfo.subscription}</p></div>
+                                <div><p>Credit Card:</p><p className="edit-field">{stripeInfo.subscription}</p></div>
+                            </div>
+                            {/**=============== Educational Research Participation Toggle  ======================= */}
+                            <div>
+                                <h2 className="ed-h2">Educational Research Information</h2>
+                            </div>
+                            {/**=============== Educational Research Information  ======================= */}
+                            <div className="span">
+                                <div><input
+                                    type='checkbox'
+                                    name='Educational-Research'
+                                    checked={checked}
+                                    onChange={handleCheck}
+                                />
+                                    <p className="check-box">I would like to participate in the Educational Research</p></div>
+                            </div>
                         </div>
                         {/**=============== Manage Profile  ======================= */}
-                        {/** <div className="flex-two">
-                            <div>
-                                <h2>Manage Profile</h2>
-                                <p>Place Holder Info</p>
-                            </div>
-                    </div>   */}
+                        <div className="flex-two">
+                            <h2>Manage Profile</h2>
+                            <button onClick={() => props.history.push('/hub')}>BACK TO GAME</button>
+                        </div>
                     </div>
-                </div>
-            }
-            <div className="right-align">
-                <button onClick={() => setChecked(false)}>Opt Out</button>
-                <button onClick={() => setEdit(true)}>Edit</button>
-                <button onClick={() => setEdit(false)}>Save</button>
+
+                }
             </div>
         </div>
     )
