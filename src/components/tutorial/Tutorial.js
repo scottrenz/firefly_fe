853f26918a774c3551f6fc64055fbd11e69cd04f
Adetunji <Ashennaike@gmail.com>
2019-11-05 19:43:20 -0500

    setup sass file

--- src/components/tutorial/Tutorial.js
+++ src/components/tutorial/Tutorial.js
@@ -6,30 +6,32 @@ export const Tutorial = () => {
     <div className = 'container'>
       <h1>Step 3: How It Works</h1>
       {/* Success Bar */}
-      <div className = 'cardContainer'>
-        <div className = 'tutorialCard'>
-          <h2 className = 'cardTitle'>Access My Account</h2>
-          {/* down arrow */}
-          <img src = '' alt = '' className = 'tutorialImage' />
+      <div className = 'tutorialContainer'>
+        <div className = 'cardContainer'>
+          <div className = 'tutorialCard'>
+            <h2 className = 'cardTitle'>Access My Account</h2>
+            {/* down arrow */}
+            <img src = '' alt = '' className = 'tutorialImage' />
+          </div>
+          <div className = 'tutorialCard'>
+            <h2 className = 'cardTitle'>Manage Account</h2>
+            <img src = '' alt = '' className = 'tutorialImage' />
+            <ol>
+              <li>Manage Profiles</li>
+              <li>Update Account Information</li>
+              <li>Manage Payment Information</li>
+            </ol>
+          </div>
+          <div className = 'tutorialCard'>
+            <h2 className = 'cardTitle'>Start Our Adventure</h2>
+            <img src = '' alt = '' className = 'tutorialImage' />
+            {/* up arrow */}
+          </div>
         </div>
-        <div className = 'tutorialCard'>
-          <h2 className = 'cardTitle'>Manage Account</h2>
-          <img src = '' alt = '' className = 'tutorialImage' />
-          <ol>
-            <li>Manage Profiles</li>
-            <li>Update Account Information</li>
-            <li>Manage Payment Information</li>
-          </ol>
+        <div className = 'btnContainer'>
+          <button className = 'whiteGreen'>Back</button>
+          <button className = 'greenWhite'>Get Started</button>
         </div>
-        <div className = 'tutorialCard'>
-          <h2 className = 'cardTitle'>Start Our Adventure</h2>
-          <img src = '' alt = '' className = 'tutorialImage' />
-          {/* up arrow */}
-        </div>
-      </div>
-      <div className = 'btnContainer'>
-        <button className = 'whiteGreen'>Back</button>
-        <button className = 'greenWhite'>Get Started</button>
       </div>
     </div>
   )
