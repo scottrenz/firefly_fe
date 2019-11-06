691f173231560748ad35a485d5c4d8d44a80e4e0
Adetunji <Ashennaike@gmail.com>
2019-11-05 19:27:33 -0500

    changed form class to functional

--- src/components/tutorial/Tutorial.js
+++ src/components/tutorial/Tutorial.js
@@ -1,42 +1,36 @@
 // Import react and any dependencies necessary 
 import React from 'react'
 
-
-class Tutorial extends Component {
-  state = {
-
-  }
-  render() {
-    return (
-      <div className = 'container'>
-        <h1>Step 3: How It Works</h1>
-        {/* Image */}
-        <div className = 'cardContainer'>
-          <div className = 'tutorialCard'>
-            <h2 className = 'cardTitle'>Access My Account</h2>
-            {/* down arrow */}
-            {/* Image */}
-          </div>
-          <div className = 'tutorialCard'>
-            <h2 className = 'cardTitle'>Manage Account</h2>
-            {/* Image */}
-            <ol>
-              <li>Manage Profiles</li>
-              <li>Update Account Information</li>
-              <li>Manage Payment Information</li>
-            </ol>
-          </div>
-          <div className = 'tutorialCard'>
-            <h2 className = 'cardTitle'>Start Our Adventure</h2>
-            {/* Image */}
-            {/* up arrow */}
-          </div>
+export const Tutorial = () => {
+  return (
+    <div className = 'container'>
+      <h1>Step 3: How It Works</h1>
+      {/* Success Bar */}
+      <div className = 'cardContainer'>
+        <div className = 'tutorialCard'>
+          <h2 className = 'cardTitle'>Access My Account</h2>
+          {/* down arrow */}
+          <img src = '' alt = '' className = 'tutorialImage' />
         </div>
-        <div className = 'btnContainer'>
-          <button className = 'whiteGreen'>Back</button>
-          <button className = 'greenWhite'>Get Started</button>
+        <div className = 'tutorialCard'>
+          <h2 className = 'cardTitle'>Manage Account</h2>
+          <img src = '' alt = '' className = 'tutorialImage' />
+          <ol>
+            <li>Manage Profiles</li>
+            <li>Update Account Information</li>
+            <li>Manage Payment Information</li>
+          </ol>
+        </div>
+        <div className = 'tutorialCard'>
+          <h2 className = 'cardTitle'>Start Our Adventure</h2>
+          <img src = '' alt = '' className = 'tutorialImage' />
+          {/* up arrow */}
         </div>
       </div>
-    )
-  }
+      <div className = 'btnContainer'>
+        <button className = 'whiteGreen'>Back</button>
+        <button className = 'greenWhite'>Get Started</button>
+      </div>
+    </div>
+  )
 }
