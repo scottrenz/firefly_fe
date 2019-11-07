32f2806e5f92a902a929a17f28f566628dbea4a9
Ryan-Wisniewski <Ryanwis98@yahoo.com>
2019-11-06 23:36:18 -0500

    added required to all the other fields

--- src/components/accountInfo/AccountInfo.js
+++ src/components/accountInfo/AccountInfo.js
@@ -13,23 +13,13 @@ export default class AccountInfo extends Component {
         state: '',
         zipCode: '',
     }
-
+    //check for first name
     handleChange = e => {
-        console.log(this.props)
-        this.setState({ 
-                ...this.state,
-                [e.target.name]: e.target.value 
-        })
-        // console.log(this.state)
-    }
-
-    //check length for address
-    handleChange2 = e => {
-        if(this.state.address.length <= 1){
+        if(this.state.firstName.length <= 1){
             let element = document.getElementById("pTag")
             element.classList.remove("hidden")
         }
-        if(this.state.address.length > 1){
+        if(this.state.firstName.length > 1){
             let element = document.getElementById("pTag")
             element.classList.add("hidden")
         }
@@ -37,17 +27,83 @@ export default class AccountInfo extends Component {
                 ...this.state,
                 [e.target.name]: e.target.value 
         })
+        // console.log(this.state)
     }
 
-    //check length for city
-    handleChange3 = e => {
-        if(this.state.city.length <= 1){
+    //check length for last name
+    handleChange2 = e => {
+        if(this.state.lastName.length <= 1){
             let element = document.getElementById("pTag2")
             element.classList.remove("hidden2")
         }
-        if(this.state.city.length > 1){
+        if(this.state.lastName.length > 1){
             let element = document.getElementById("pTag2")
             element.classList.add("hidden2")
+        }
+        this.setState({ 
+                ...this.state,
+                [e.target.name]: e.target.value 
+        })
+    }
+
+    //check length for address
+    handleChange3 = e => {
+        if(this.state.address.length <= 1){
+            let element = document.getElementById("pTag3")
+            element.classList.remove("hidden3")
+        }
+        if(this.state.address.length > 1){
+            let element = document.getElementById("pTag3")
+            element.classList.add("hidden3")
+        }
+        this.setState({ 
+                ...this.state,
+                [e.target.name]: e.target.value 
+        })
+    }
+
+    //for city
+    handleChange4 = e => {
+        if(this.state.city.length <= 1){
+            let element = document.getElementById("pTag4")
+            element.classList.remove("hidden4")
+        }
+        if(this.state.city.length > 1){
+            let element = document.getElementById("pTag4")
+            element.classList.add("hidden4")
+        }
+        this.setState({ 
+                ...this.state,
+                [e.target.name]: e.target.value 
+        })
+    }
+
+
+    //for state of residence
+    handleChange5 = e => {
+        if(this.state.state.length <= 1){
+            let element = document.getElementById("pTag5")
+            element.classList.remove("hidden5")
+        }
+        if(this.state.state.length > 1){
+            let element = document.getElementById("pTag5")
+            element.classList.add("hidden5")
+        }
+        this.setState({ 
+                ...this.state,
+                [e.target.name]: e.target.value 
+        })
+    }
+
+    //for zipCode
+    handleChange6 = e => {
+        if(this.state.zipCode.length <= 1){
+            let element = document.getElementById("pTag6")
+            element.classList.remove("hidden6")
+        }
+        if(this.state.zipCode.length > 1){
+            let element = document.getElementById("pTag6")
+            element.classList.add("hidden6")
         }
         this.setState({ 
                 ...this.state,
@@ -87,7 +143,6 @@ export default class AccountInfo extends Component {
         return (
         <UserContext.Consumer>
             {props => {
-                // console.log(props)
                 
                 this.props = {...this.props, props}
                 console.log(this.props)
@@ -108,6 +163,7 @@ export default class AccountInfo extends Component {
                                     value={this.state.firstName}
                                     onChange={this.handleChange}
                                 />
+                                <p id='pTag' class='required hidden'>*Required</p>
                             </div>
                             <div className='inputColumn width2'>
                                 <label className='formLabel'>LAST NAME</label>
@@ -116,8 +172,9 @@ export default class AccountInfo extends Component {
                                     type='text'
                                     name='lastName'
                                     value={this.state.lastName}
-                                    onChange={this.handleChange}
+                                    onChange={this.handleChange2}
                                 />
+                                <p id='pTag2' class='required hidden2'>*Required</p>
                             </div>
                         </div>
                         
@@ -129,9 +186,9 @@ export default class AccountInfo extends Component {
                                     type='text'
                                     name='address'
                                     value={this.state.address}
-                                    onChange={this.handleChange2}
+                                    onChange={this.handleChange3}
                                 />
-                                <p id='pTag' class='required hidden'>*Required</p>
+                                <p id='pTag3' class='required hidden3'>*Required</p>
                             </div>
                         </div>
 
@@ -143,10 +200,10 @@ export default class AccountInfo extends Component {
                                     type='text'
                                     name='city'
                                     value={this.state.city}
-                                    onChange={this.handleChange3}
+                                    onChange={this.handleChange4}
                                     
                                 />
-                                <p id='pTag2' class='required hidden2'>*Required</p>
+                                <p id='pTag4' class='required hidden4'>*Required</p>
                             </div>
                         </div>
 
@@ -154,22 +211,24 @@ export default class AccountInfo extends Component {
                             <div className='inputColumn width2'>
                                 <label className='formLabel'>STATE</label>
                                 <input 
-                                    className='inputStyle width3'
+                                    className='inputStyle width5'
                                     type='text'
                                     name='state'
                                     value={this.state.state}
-                                    onChange={this.handleChange}
+                                    onChange={this.handleChange5}
                                 />
+                                <p id='pTag5' class='required hidden5'>*Required</p>
                             </div>
                             <div className='inputColumn width2'>
                                 <label class='formLabel'>ZIPCODE</label>
                                 <input 
-                                    className='inputStyle width3'
+                                    className='inputStyle width'
                                     type='text'
                                     name='zipCode'
                                     value={this.state.zipCode}
-                                    onChange={this.handleChange}
+                                    onChange={this.handleChange6}
                                 />
+                                <p id='pTag6' class='required hidden6'>*Required</p>
                             </div>
                         </div>
                         <div className='buttonSpace width'>
