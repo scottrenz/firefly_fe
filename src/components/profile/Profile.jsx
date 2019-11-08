482cb73b8fd134a842fc0a728198034a3d25604f
Timothy McGowen <mcgowen17@yahoo.com>
2019-11-08 13:13:00 -0700

    asdfas

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -132,7 +132,7 @@ const Profile = (props) => {
                                 </div>
                                 <div className="span">
                                     <div><p>Email:</p><p className="edit-field">{user.email}</p></div>
-                                    <div><p>Name:</p><p className="edit-field">{user.credentials.firstName}{" "}{user.credentials.LastName}</p></div>
+                                    <div><p>Name:</p><p className="edit-field">{user.credentials.firstName}{" "}{user.credentials.lastName}</p></div>
                                     <div><p>Password:</p> <p className="edit-field"></p></div>
                                 </div>
                             </div>
