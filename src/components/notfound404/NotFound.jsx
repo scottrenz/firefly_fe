e34f08b1d8064ba9e58b68432cb1d8fe4c2d7798
Arvin Agas <arvagas@gmail.com>
2019-11-12 14:17:34 -0500

    error 404 adjustments

--- src/components/notfound404/NotFound.jsx
+++ src/components/notfound404/NotFound.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './NotFound.scss'
 
-export default function NotFound() {
+export default function NotFound(props) {
 	return (
 		<div className='error'>
 			<h1>404</h1>
@@ -11,7 +11,7 @@ export default function NotFound() {
 				highly trained monkeys to solve it.
 			</p>
 			<div>
-				<button className='cta-button'>Home</button>		
+				<button className='cta-button' onClick={() => props.history.push('/')}>Home</button>		
 			</div>
 		</div>
 	)
