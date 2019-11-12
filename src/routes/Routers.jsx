8ed6811e524605113154ce32def8378425ff6c31
Arvin Agas <arvagas@gmail.com>
2019-11-12 15:00:28 -0500

    context gets populated if token already exists

--- src/routes/Routers.jsx
+++ src/routes/Routers.jsx
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from 'react';
 import { withRouter, Switch, Route } from 'react-router-dom';
 import ReactGA from 'react-ga';
 import { createBrowserHistory } from 'history';
+import axios from 'axios'
+import jwtDecode from 'jwt-decode'
 // route components
 import Signin from '../components/signin/Signin';
 import Signup from '../components/signup/Signup';
@@ -29,6 +31,17 @@ const Routers = ({ location }) => {
 	const currentKey = location.pathname.split('/')[1] || '/';
 	const timeout = { enter: 1300, exit: 200 };
 	const [loggedInUser, setLoggedInUser] = useState({});
+
+	// populate the user context if token exists
+	useEffect(() => {
+		if (localStorage.getItem('token')) {
+			const decoded = jwtDecode(localStorage.getItem('token'))
+			axios
+			.get(`https://infinite-meadow-87721.herokuapp.com/users/${decoded.subject}`)
+			.then(res => setLoggedInUser(res.data))
+			.catch(err => console.log('There was an error verifying the stored token.'))
+		}
+	}, [])
 
 	// will only run once to grab initial page start and will run again for hard refreshes; all other tracking for page changes occurs innately
 	useEffect(() => {
